cc169646efd4e7dee26b40dddc03a694862951fb
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-02 13:58:24 +0700

    CRUD User

--- backend/src/main/java/tuGreaterBackend/cn333/backend/controller/UsersController.java
+++ backend/src/main/java/tuGreaterBackend/cn333/backend/controller/UsersController.java
@@ -1,17 +1,25 @@
 package tuGreaterBackend.cn333.backend.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tuGreaterBackend.cn333.backend.entity.Users;
 import tuGreaterBackend.cn333.backend.service.UsersService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import tuGreaterBackend.cn333.backend.entity.Users;
+
+
 
 
 
@@ -26,9 +34,76 @@ public class UsersController {
 
     @GetMapping("")
     public ResponseEntity<?> getUsers() {
-        List<Users> users = usersService.getUsers();
-
-        return ResponseEntity.ok(users);
+        try {
+            List<Users> users = usersService.getUsers();
+            if(users.isEmpty()){
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            return ResponseEntity.ok(users);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+        
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable String id) {
+        try {
+            Users user = usersService.getUserById(id);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "User not found by id: " + id));
+        }
+        return ResponseEntity.ok(user);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+        
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> postUser(@RequestBody Users user) {
+        try {
+            Users newUser = usersService.saveUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(Map.of("message", "User created", "user", newUser));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putUser(@PathVariable String id, @RequestBody Users user) {
+        
+        try {
+            Users updateUser = usersService.updateUser(id,user);
+            if (updateUser == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(Map.of("message", "User not found by id: " + id));
+            }
+            return ResponseEntity.ok(updateUser);
+                    
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable String id) {
+        try {
+            usersService.deleteUser(id);
+            return ResponseEntity.ok(Map.of("message", "User deleted successfully"));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    
+
+    
     
 }
--- backend/src/main/java/tuGreaterBackend/cn333/backend/service/UsersService.java
+++ backend/src/main/java/tuGreaterBackend/cn333/backend/service/UsersService.java
@@ -18,8 +18,61 @@ public class UsersService {
         this.usersRepository = usersRepository;
     }
 
-    public List<Users> getUsers(){
-        return usersRepository.findAll();
+    public List<Users> getUsers() throws Exception{
+        try {
+            return usersRepository.findAll();
+        } catch (Exception e) {
+            throw  new Exception("An error occurred while fetching users " ,e);
+        }
+    }
+
+    public Users getUserById(String id) throws Exception{
+        try {
+            return usersRepository.findById(id).orElse(null);
+        } catch (Exception e) {
+            throw  new Exception("An error occurred while fetching user by id: " + id ,e);
+        }
+    }
+
+    public Users saveUser(Users user) throws Exception{
+        try {
+            return usersRepository.save(user);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Failed to create new user", e);
+        } catch (Exception e) {
+            throw  new Exception("An error occurred while saving user ",e);
+        }
+        
+    }
+
+    public void deleteUser(String id) throws Exception{
+        try {
+            usersRepository.deleteById(id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Failed to delete new user", e);
+        } catch (Exception e) {
+            throw  new Exception("An error occurred while deleting user ",e);
+        }
+    }
+
+    public Users updateUser(String id,Users user) throws Exception{
+        try {
+            Users existingUser = usersRepository.findById(id).orElse(null);
+            if (existingUser!= null) {
+                existingUser.setUsername(user.getUsername());
+                existingUser.setPassword(user.getPassword());
+                existingUser.setDisplayName(user.getDisplayName());
+                existingUser.setProfileImageUrl(user.getProfileImageUrl());
+                existingUser.setRole(user.getRole());
+                existingUser.setStudentId(user.getStudentId());
+                return usersRepository.save(existingUser);
+            }else{
+                return null;
+            }
+        } catch (Exception e) {
+            throw  new Exception("An error occurred while updating user ",e);
+        }
+        
     }
 
 }
