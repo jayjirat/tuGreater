0576b03bcb7ab758f4244706d99565744061ae3f
Jirat Charoenkaew <jirat8098@gmail.com>
2025-04-09 17:10:53 +0700

    Adjust likeController

--- backend/src/main/java/tuGreaterBackend/cn333/backend/controller/LikeController.java
+++ backend/src/main/java/tuGreaterBackend/cn333/backend/controller/LikeController.java
@@ -1,21 +1,16 @@
 package tuGreaterBackend.cn333.backend.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import tuGreaterBackend.cn333.backend.service.LikeService;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import tuGreaterBackend.cn333.backend.entity.Like;
-
-
-
+import tuGreaterBackend.cn333.backend.service.LikeService;
 
 @RestController
 @RequestMapping("/community/like")
