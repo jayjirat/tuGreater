3ef6cbb700961d6193d711ba3fae054494179dfc
Jirat Charoenkaew <jirat8098@gmail.com>
2025-04-15 16:05:59 +0700

    Add create report backend

---
+++ backend/src/main/java/tuGreaterBackend/cn333/backend/controller/ReportController.java
@@ -0,0 +1,33 @@
+package tuGreaterBackend.cn333.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tuGreaterBackend.cn333.backend.entity.Report;
+import tuGreaterBackend.cn333.backend.service.ReportService;
+
+
+@RestController
+@RequestMapping("/report")
+public class ReportController {
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> postReport(@RequestBody Report report) {
+        try {
+            Report newReport = reportService.createReport(report);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newReport);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+    
+}
---
+++ backend/src/main/java/tuGreaterBackend/cn333/backend/service/ReportService.java
@@ -0,0 +1,26 @@
+package tuGreaterBackend.cn333.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tuGreaterBackend.cn333.backend.entity.Report;
+import tuGreaterBackend.cn333.backend.repository.ReportRepository;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private final ReportRepository reportRepository;
+
+    public ReportService(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    public Report createReport(Report report) {
+        try {
+            return reportRepository.save(report);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create new post", e);
+        }
+    }
+}
