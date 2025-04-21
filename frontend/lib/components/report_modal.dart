f26031992fa4a2a7322bd7e4b7309fc46f330719
Melnyks <wern2004man@hotmail.com>
2025-04-21 22:26:42 +0700

    make report modal can scroll to submit

--- frontend/lib/components/report_modal.dart
+++ frontend/lib/components/report_modal.dart
@@ -28,158 +28,163 @@ class _ReportModalState extends ConsumerState<ReportModal> {
     return Container(
       height: 600,
       padding: EdgeInsets.all(24),
-      child: SingleChildScrollView(
-        child: Align(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Text(
-                AppLocalizations.of(context)!.report_product,
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Row(
+      child: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.report_product,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                          value: isChecked1,
+                          onChanged: (value) {
+                            setState(() {
+                              isChecked1 = value!;
+                              if (isChecked1) {
+                                reportReasons.add("Illegal Product");
+                              } else {
+                                reportReasons.remove("Illegal Product");
+                              }
+                            });
+                          }),
+                      Text(AppLocalizations.of(context)!.report_illegal_product)
+                    ],
+                  ),
+                ),
+                Row(
                   children: [
                     Checkbox(
-                        value: isChecked1,
+                        value: isChecked2,
                         onChanged: (value) {
                           setState(() {
-                            isChecked1 = value!;
-                            if (isChecked1) {
-                              reportReasons.add("Illegal Product");
+                            isChecked2 = value!;
+                            if (isChecked2) {
+                              reportReasons.add("Description does not match");
                             } else {
-                              reportReasons.remove("Illegal Product");
+                              reportReasons
+                                  .remove("Description does not match");
                             }
                           });
                         }),
-                    Text(AppLocalizations.of(context)!.report_illegal_product)
+                    Text(AppLocalizations.of(context)!
+                        .report_item_does_not_match_the_description)
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                      value: isChecked2,
-                      onChanged: (value) {
-                        setState(() {
-                          isChecked2 = value!;
-                          if (isChecked2) {
-                            reportReasons.add("Description does not match");
-                          } else {
-                            reportReasons.remove("Description does not match");
-                          }
-                        });
-                      }),
-                  Text(AppLocalizations.of(context)!
-                      .report_item_does_not_match_the_description)
-                ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                      value: isChecked3,
-                      onChanged: (value) {
-                        setState(() {
-                          isChecked3 = value!;
-                          if (isChecked3) {
-                            reportReasons.add("Did not deliver the item");
-                          } else {
-                            reportReasons.remove("Did not deliver the item");
-                          }
-                        });
-                      }),
-                  Text(AppLocalizations.of(context)!
-                      .report_seller_did_not_deliver_the_item)
-                ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                      value: isChecked4,
-                      onChanged: (value) {
-                        setState(() {
-                          isChecked4 = value!;
-                          if (isChecked4) {
-                            reportReasons.add("Other");
-                          } else {
-                            reportReasons.remove("Other");
-                          }
-                        });
-                      }),
-                  Text(
-                      AppLocalizations.of(context)!.report_Other_please_specify)
-                ],
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.report_description,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: SizedBox(
-                    height: 150,
-                    child: TextField(
-                      maxLines: null,
-                      minLines: 5,
-                      controller: descriptionController,
-                      decoration: InputDecoration(
-                        hintText: AppLocalizations.of(context)!
-                            .report_description_placeholder,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: BorderSide.none,
+                Row(
+                  children: [
+                    Checkbox(
+                        value: isChecked3,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked3 = value!;
+                            if (isChecked3) {
+                              reportReasons.add("Did not deliver the item");
+                            } else {
+                              reportReasons.remove("Did not deliver the item");
+                            }
+                          });
+                        }),
+                    Text(AppLocalizations.of(context)!
+                        .report_seller_did_not_deliver_the_item)
+                  ],
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                        value: isChecked4,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked4 = value!;
+                            if (isChecked4) {
+                              reportReasons.add("Other");
+                            } else {
+                              reportReasons.remove("Other");
+                            }
+                          });
+                        }),
+                    Text(AppLocalizations.of(context)!
+                        .report_Other_please_specify)
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.report_description,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: SizedBox(
+                      height: 150,
+                      child: TextField(
+                        maxLines: null,
+                        minLines: 5,
+                        controller: descriptionController,
+                        decoration: InputDecoration(
+                          hintText: AppLocalizations.of(context)!
+                              .report_description_placeholder,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[200],
                         ),
-                        filled: true,
-                        fillColor: Colors.grey[200],
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25),
-                child: FilledButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.red),
-                    ),
-                    onPressed: () async {
-                      await ref.read(reportProvider.notifier).createReport(
-                          reportReasons: reportReasons,
-                          additionalInfo: descriptionController.text,
-                          reportedBy: user!.studentId,
-                          postId: productId,
-                          postCategory: PostCategory.shop);
-                      if (context.mounted) {
-                        Navigator.pop(context);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text("Report submitted!"),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text(
-                      AppLocalizations.of(context)!.report_submit,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.only(top: 25),
+                  child: FilledButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
                       ),
-                    )),
-              )
-            ],
+                      onPressed: () async {
+                        await ref.read(reportProvider.notifier).createReport(
+                            reportReasons: reportReasons,
+                            additionalInfo: descriptionController.text,
+                            reportedBy: user!.studentId,
+                            postId: productId,
+                            postCategory: PostCategory.shop);
+                        if (context.mounted) {
+                          Navigator.pop(context);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("Report submitted!"),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.report_submit,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      )),
+                )
+              ],
+            ),
           ),
         ),
       ),
