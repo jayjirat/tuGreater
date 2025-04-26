43d20bd299d863112b2a9a25451e4de36728c40e
DinoChicken <Dinosuteethorni@gmail.com>
2025-04-26 15:51:28 +0700

    admin can view post before delete

--- frontend/lib/screens/admin/reported_post.dart
+++ frontend/lib/screens/admin/reported_post.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/providers/report_provider.dart';
 import 'package:frontend/models/report.dart';
+import 'package:frontend/screens/community/community_view_post.dart';
+import 'package:frontend/screens/shop/item_detail.dart';
 
 class ReportedPost extends ConsumerStatefulWidget {
   const ReportedPost({super.key});
@@ -47,48 +49,63 @@ class ReportState extends ConsumerState<ReportedPost> {
                         Text("Reported By: ${report.reportedBy}"),
                       ],
                     ),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                      onTap: () async {
-                        final confirm = await showDialog<bool>(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text("Delete Post"),
-                            content: Text("Are you sure you want to delete this post?"),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(false),
-                                child: Text("Cancel"),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(true),
-                                child: Text("Delete"),
-                              ),
-                            ],
-                          ),
-                        );
+                    trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () async {
+                      final confirm = await showDialog<bool>(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text("Delete Post"),
+                          content: Text("Are you sure you want to delete this post?"),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(false),
+                              child: Text("Cancel"),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(true),
+                              child: Text("Delete"),
+                            ),
+                          ],
+                        ),
+                      );
 
-                        if (confirm == true) {
-                          try {
-                            if (report.postCategory == PostCategory.community) {
-                              await ref.read(reportProvider.notifier).deleteCommunityPost(report.postId);
-                            } else if(report.postCategory == PostCategory.shop) {
-                              await ref.read(reportProvider.notifier).deleteProductPost(report.postId);
-                            }
-                            await ref.read(reportProvider.notifier).deleteReport(report.id);
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text("post deleted")),
-                            );
-                          } catch (e) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text("Failed to delete report: $e")),
-                            );
-                          }
-                        }
-                      },
-                  ),
-                );
-              },
-            ),
+                  if (confirm == true) {
+                    try {
+                      if (report.postCategory == PostCategory.community) {
+                        await ref.read(reportProvider.notifier).deleteCommunityPost(report.postId);
+                      } else if(report.postCategory == PostCategory.shop) {
+                        await ref.read(reportProvider.notifier).deleteProductPost(report.postId);
+                      }
+                      await ref.read(reportProvider.notifier).deleteReport(report.id);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("post deleted")),
+                      );
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Failed to delete report: $e")),
+                      );
+                    }
+                  }
+                },
+              ),
+              onTap: () {
+                if (report.postCategory == PostCategory.community) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CommunityViewpost(id: report.postId)),
+                  );
+                } else if(report.postCategory == PostCategory.shop) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ItemDetail(productId: report.postId,)),
+                  );
+                }
+              }
+            )
+          );
+        },
+      ),
     );
   }
 
